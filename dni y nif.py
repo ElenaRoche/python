9ca97666c448a49ead84a9d535eefad8dd6d3bdb
ElenaRoche <33655419+ElenaRoche@users.noreply.github.com>
2017-12-01 12:33:04 +0100

    Add files via upload

---
+++ dni y nif.py
@@ -0,0 +1,7 @@
+def nif():
+    dni=input("Dime tu dni")
+    resto=dni%23
+    letras=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
+    print letras[resto]
+nif()
+    
